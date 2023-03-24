f8263e6637365c18366d6cf355948161ba7c62b4
Orchid Aung <orchid.lso@gmail.com>
2023-03-24 19:14:03 +0800

    with reference images predict

--- run_parallel.py
+++ run_parallel.py
@@ -4,10 +4,63 @@ from gtts import gTTS
 from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import pyttsx3
 import time
+import ReferenceImageVal as ri
+import signal
+
+
+model = YOLO('yolov8n.pt')  #yolov8n.pt load a pretrained model (recommended for training)
+
+
+def DetectReferenceImages():
+    #for i in range(80):
+    result = model.predict(source='ReferenceImages/person.png')
+    ri.person_width_in_rf = result[0].boxes.xywh[0][2]
+    print(f'-----------Person width : {ri.person_width_in_rf}')
+
+    result_cellphone= model.predict(source='ReferenceImages/cellphone.png')
+    ri.mobile_width_in_rf = result_cellphone[0].boxes.xywh[0][2]
+    print(f'-----------Cellphone width : {ri.mobile_width_in_rf}')
+
+    result_handbag = model.predict(source='ReferenceImages/handbag.jpeg')
+    ri.handbag_width_in_rf = result_handbag[0].boxes.xywh[0][2]
+    print(f'-----------Handbag width : {ri.handbag_width_in_rf}')
+
+    result_mouse = model.predict(source='ReferenceImages/mouse.jpeg')
+    ri.mouse_width_in_rf = result_mouse[0].boxes.xywh[0][2]
+    print(f'-----------Mouse width : {ri.mouse_width_in_rf}')
+
 
 def Detect():
-    model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
-    results = model.predict(source="0", show = True)
+
+    result = model.predict(source='ReferenceImages/person.png')
+    ri.person_width_in_rf = result[0].boxes.xywh[0][2]
+    print(f'-----------Person width : {ri.person_width_in_rf}')
+
+    result_cellphone= model.predict(source='ReferenceImages/cellphone.png')
+    ri.mobile_width_in_rf = result_cellphone[0].boxes.xywh[0][2]
+    print(f'-----------Cellphone width : {ri.mobile_width_in_rf}')
+
+    result_handbag = model.predict(source='ReferenceImages/handbag.jpeg')
+    ri.handbag_width_in_rf = result_handbag[0].boxes.xywh[0][2]
+    print(f'-----------Handbag width : {ri.handbag_width_in_rf}')
+
+    result_mouse = model.predict(source='ReferenceImages/mouse.jpeg')
+    ri.mouse_width_in_rf = result_mouse[0].boxes.xywh[0][2]
+    print(f'-----------Mouse width : {ri.mouse_width_in_rf}')
+
+    result_bottle = model.predict(source='ReferenceImages/bottle.jpeg')
+    ri.bottle_width_in_rf = result_bottle[0].boxes.xywh[0][2]
+    print(f'-----------Bottle width : {ri.bottle_width_in_rf}')
+
+    result_backpack = model.predict(source='ReferenceImages/backpack.jpeg')
+    ri.backpack_width_in_rf = result_backpack[0].boxes.xywh[0][2]
+    print(f'-----------Backpack width : {ri.backpack_width_in_rf}')
+
+    result_laptop = model.predict(source='ReferenceImages/laptop.jpeg')
+    ri.laptop_width_in_rf = result_laptop[0].boxes.xywh[0][2]
+    print(f'-----------Laptop width : {ri.laptop_width_in_rf}')
+
+    model.predict(source="0", show = True)
 
 def Speech():
     engine = pyttsx3.init()
@@ -24,10 +77,25 @@ def Speech():
         time.sleep(3)
 
 
+
+p1 = Process(target= Detect)
+p2 = Process(target= Speech)
+
+def stopProcess():
+    p1.kill()
+    p2.kill()
+    print('--------------EXIT START------------------')
+    signal.SIGINT
+    print('--------------EXIT END------------------')
+
 if __name__ == '__main__':
-  p1 = Process(target=Detect)
+
   p1.start()
-  p2 = Process(target=Speech)
   p2.start()
-  p1.join()
-  p2.join()
+  #time.sleep(30)
+
+
+  #stopProcess()
+
+ # p1.join()
+ # p2.join()
