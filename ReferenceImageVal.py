78a277de4f564adc6ad8ffc0984cd177afcd07a1
Orchid Aung <orchid.lso@gmail.com>
2023-03-24 19:15:48 +0800

    Reference image width py

---
+++ ReferenceImageVal.py
@@ -0,0 +1,16 @@
+person_width_in_rf = 0
+mobile_width_in_rf = 0
+backpack_width_in_rf = 0
+handbag_width_in_rf = 0
+bottle_width_in_rf = 0
+chair_width_in_rf = 0
+laptop_width_in_rf = 0
+dining_table_in_rf = 0
+laptop_width_in_rf = 0
+mouse_width_in_rf = 0
+keyboard_width_in_rf = 0
+book_width_in_rf = 0
+
+def setval(x,y):
+    person_width_in_rf, mobile_width_in_rf = x,y
+    return person_width_in_rf,mobile_width_in_rf
