87457b509e4605e08921f504f5e0f3bc16b2df2f
Tan Hui Min <hmqui90@gmail.com>
2023-03-26 15:15:23 +0800

    Update streamlit_run_parallel.py
    Updated on loadstream wrappers

--- streamlit_run_parallel.py
+++ streamlit_run_parallel.py
@@ -12,7 +12,7 @@ import time
 import logging
 import cv2
 
-from ultralytics.yolo.data.dataloaders.stream_loaders import (LoadStreams)
+from ultralytics.yolo.data.dataloaders.stream_loaders import (LoadStreams, LoadStreamsWrapper)
 
 
 model = YOLO('yolov8n.pt')  #yolov8n.pt load a pretrained model (recommended for training)
@@ -46,37 +46,38 @@ def detect():
     logging.warning ('-----------------Detect started---------------------')
     st.write('test_fun started')
 #     for img in loader.main():
+    stream = LoadStreamsWrapper()
+    for frame in stream:
+        result = model.predict(source='ReferenceImages/person.png')
+        ri.person_width_in_rf = result[0].boxes.xywh[0][2]
+        print(f'-----------Person width : {ri.person_width_in_rf}')
+        st.write(f'-----------Person width : {ri.person_width_in_rf}')
 
-    result = model.predict(source='ReferenceImages/person.png')
-    ri.person_width_in_rf = result[0].boxes.xywh[0][2]
-    print(f'-----------Person width : {ri.person_width_in_rf}')
-    st.write(f'-----------Person width : {ri.person_width_in_rf}')
+        result_cellphone= model.predict(source='ReferenceImages/cellphone.png')
+        ri.mobile_width_in_rf = result_cellphone[0].boxes.xywh[0][2]
+        print(f'-----------Cellphone width : {ri.mobile_width_in_rf}')
 
-    result_cellphone= model.predict(source='ReferenceImages/cellphone.png')
-    ri.mobile_width_in_rf = result_cellphone[0].boxes.xywh[0][2]
-    print(f'-----------Cellphone width : {ri.mobile_width_in_rf}')
+        result_handbag = model.predict(source='ReferenceImages/handbag.jpeg')
+        ri.handbag_width_in_rf = result_handbag[0].boxes.xywh[0][2]
+        print(f'-----------Handbag width : {ri.handbag_width_in_rf}')
 
-    result_handbag = model.predict(source='ReferenceImages/handbag.jpeg')
-    ri.handbag_width_in_rf = result_handbag[0].boxes.xywh[0][2]
-    print(f'-----------Handbag width : {ri.handbag_width_in_rf}')
+        result_mouse = model.predict(source='ReferenceImages/mouse.jpeg')
+        ri.mouse_width_in_rf = result_mouse[0].boxes.xywh[0][2]
+        print(f'-----------Mouse width : {ri.mouse_width_in_rf}')
 
-    result_mouse = model.predict(source='ReferenceImages/mouse.jpeg')
-    ri.mouse_width_in_rf = result_mouse[0].boxes.xywh[0][2]
-    print(f'-----------Mouse width : {ri.mouse_width_in_rf}')
+        result_bottle = model.predict(source='ReferenceImages/bottle.jpeg')
+        ri.bottle_width_in_rf = result_bottle[0].boxes.xywh[0][2]
+        print(f'-----------Bottle width : {ri.bottle_width_in_rf}')
 
-    result_bottle = model.predict(source='ReferenceImages/bottle.jpeg')
-    ri.bottle_width_in_rf = result_bottle[0].boxes.xywh[0][2]
-    print(f'-----------Bottle width : {ri.bottle_width_in_rf}')
+        result_backpack = model.predict(source='ReferenceImages/backpack.jpeg')
+        ri.backpack_width_in_rf = result_backpack[0].boxes.xywh[0][2]
+        print(f'-----------Backpack width : {ri.backpack_width_in_rf}')
 
-    result_backpack = model.predict(source='ReferenceImages/backpack.jpeg')
-    ri.backpack_width_in_rf = result_backpack[0].boxes.xywh[0][2]
-    print(f'-----------Backpack width : {ri.backpack_width_in_rf}')
+        result_laptop = model.predict(source='ReferenceImages/laptop.jpeg')
+        ri.laptop_width_in_rf = result_laptop[0].boxes.xywh[0][2]
+        print(f'-----------Laptop width : {ri.laptop_width_in_rf}')
 
-    result_laptop = model.predict(source='ReferenceImages/laptop.jpeg')
-    ri.laptop_width_in_rf = result_laptop[0].boxes.xywh[0][2]
-    print(f'-----------Laptop width : {ri.laptop_width_in_rf}')
-
-    model.predict(source = img, show = True)
+        model.predict(source = frame, show = True)
 
 # def test_fun():
 #     logging.warning ('-----------------Detect started---------------------')
