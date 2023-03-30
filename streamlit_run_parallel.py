8c78f2b990b536fd22ecf23138c50820c75bf08b
Orchid Aung <orchid.lso@gmail.com>
2023-03-31 02:00:37 +0800

    put st.audio outside of the method

--- streamlit_run_parallel.py
+++ streamlit_run_parallel.py
@@ -196,8 +196,11 @@ def autoplay_audio(file_path: str):
          ).write("# Auto-playing Audio!")
 
 
+audio_file = open('myaudio.mp3', 'rb')
+audio_bytes = audio_file.read()
 
-st.audio(sound_file)
+st.audio(audio_bytes, format='audio/mp3')
+
 # def stop_process(self):
 #     self.kill()
 # p1 = Process(target= Detect)
