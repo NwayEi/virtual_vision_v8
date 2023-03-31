03cf3fd6676a2f90f804bc805dd452f766823edf
Tan Hui Min <hmqui90@gmail.com>
2023-03-31 09:33:50 +0800

    commit predict

--- ultralytics/yolo/v8/detect/predict.py
+++ ultralytics/yolo/v8/detect/predict.py
@@ -6,7 +6,7 @@ from ultralytics.yolo.engine.predictor import BasePredictor
 from ultralytics.yolo.engine.results import Results
 from ultralytics.yolo.utils import DEFAULT_CFG, ROOT, ops
 from ultralytics.yolo.utils.plotting import Annotator, colors, save_one_box
-# from gtts import gTTS
+from gtts import gTTS
 import ReferenceImageVal as rf
 new_text =''
 old_text = ''
@@ -251,14 +251,14 @@ class DetectionPredictor(BasePredictor):
             file.close()
 
             print (f'------------------WRITE FILE {self.new_text}---------------------------')
-            #Generate mp3 file
-            # gtts = gTTS(self.new_text, lang='en')
-            # gtts.save('myaudio.mp3')
+            # Generate mp3 file
+            gtts = gTTS(self.new_text, lang='en')
+            gtts.save('myaudio.mp3')
 
-            #reset the text
-            # if self.new_text != '':
-                # gtts = gTTS(self.new_text, lang='en')
-                # gtts.save('myaudio.mp3')
+            # reset the text
+            if self.new_text != '':
+                gtts = gTTS(self.new_text, lang='en')
+                gtts.save('myaudio.mp3')
             self.new_text = ''
 
 
